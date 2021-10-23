84df15270f308feb6c683706deabf1221000e0a4
sdfunni <60749073+sdfunni@users.noreply.github.com>
2021-10-23 11:32:42 -0400

    Update SingleOrigin_script_ZnO.py

--- SingleOrigin_script_ZnO.py
+++ SingleOrigin_script_ZnO.py
@@ -16,23 +16,25 @@ from SingleOrigin import *
 #%%
 """Location of image data"""
 
-path = '/Users/funni/Downloads/ZnO_dDPC.tif'
+path = 'STEM_Images/dDPC_ZnO.tif'
+# path = 'STEM_Images/1926 20210726 DF4 7.60 Mx ZnO_3 iDPC_SDCorr.tif'
 
 #%%
 """Structure import from .cif and project"""
 
-cif_path = 'ZnO_mp-2133_symmetrized.cif'
+# cif_path = 'ZnO_mp-2133_symmetrized.cif'
+# cif_path = 'ZnO_drprobe.cif'
+cif_path = 'ZnO.cif'
 
 za = [1,1,0]            #Zone Axis direction
-a1 = [1,-1,0]         #Apparent horizontal axis in projection
-a2 = [0,0,1]          #Most vertical axis in projection
-
+a2 = [-1,1,0]         #Apparent horizontal axis in projection
+a3 = [0,0,1]          #Most vertical axis in projection
 #%%
 """Find 3D coordinates of unit cell, metric tensor and 3D direct 
 stucture matrix (a_3d)"""
 uc = UnitCell(cif_path)
 
-uc.transform_basis(za, a1, a2)
+uc.transform_basis(za, a2, a3)
 
 """Project Unit Cell to 2D and combine coincident/proximate columns"""
 uc.project_uc_2d(proj_axis = 0, ignore_elements = []) 
@@ -46,21 +48,16 @@ uc.plot_unit_cell()
 image = import_image(path, display_image=True)
 
 #%%
-"""Define offset (in fractional coordinates from the unit cell origin) 
-of an easily picked atom column"""
-
-basis_offset = uc.at_cols.loc[3, 'u':'v'].tolist()
-
 """Initialize AtomicColumnLattice object"""
 
-acl = AtomicColumnLattice(image, uc, resolution=0.8,
-                          basis_offset_frac=basis_offset)
+acl = AtomicColumnLattice(image, uc, resolution=0.8, 
+                          xlim=None, ylim=None)
 
 """Get real space basis vectors using the FFT
 if some FFT peaks are weak or absent (such as forbidden reflections), 
 specify the order of the first peak that is clearly visible"""
 
-acl.fft_get_basis_vect(a1_order=1, a2_order=2, sigma=2)
+acl.fft_get_basis_vect(a1_order=1, a2_order=2, sigma=1.5)
 
 #%%
 """Generate a mathematical lattice from the basis vectors and unit cell 
@@ -68,44 +65,49 @@ acl.fft_get_basis_vect(a1_order=1, a2_order=2, sigma=2)
 
 acl.define_reference_lattice()
 
+#%%
 """Fit atom columns at reference lattice points
 -Automatically decides what filters to use based on resolution (input above) 
 and pixel size (determined from basis vectors lengths compared to .cif lattice 
-parameters"""
+parameters
+-If fitting is extremely slow due to simultaneous fitting, set 
+"Gauss_sigma=None". Be careful to results (including residuals) to verify 
+accuracy."""
 
-acl.fit_atom_columns(edge_max_threshold=0.95, buffer=40)
+acl.fit_atom_columns(edge_max_threshold=1.1, buffer=0, Gauss_sigma='auto')
 
-#%%
+ #%%
 """Use the fitted atomic column positions to refine the basis vectors and 
     origin. 
     -It is best to choose a well defined sublattice with minimal 
     displacements. """
- 
-acl.refine_reference_lattice('elem', 'Zn')
-
+    
+acl.refine_reference_lattice('elem', 'Zn', outliers=30)
 
 #%%
 """Check image residuals after fitting"""
-""" *** This is what I was talking about. I need to make a function that 
-does this, but better. """
+"""Plots image residuals after fitting according to the following:
+    1) subtracts fitted gaussians from image intensity.
+    2) then applies masks used for fitting.
+    3) background intensity values from the gaussian fits are not subtracted.
+    Should look for small and relatively flat 
+    """
 
-residuals = copy.deepcopy(acl.image)
-y, x = np.indices(acl.image.shape)
-for row in acl.at_cols.itertuples():
-    peak = gaussian_2d(x, y, row.x_fit, row.y_fit ,row.sig_maj,
-                             row.sig_rat, -row.theta, row.peak_int, 
-                             0)
-    residuals -= peak
+acl.plot_fitting_residuals()
 
-plt.figure()
-plt.imshow(residuals*acl.all_masks)
-plt.scatter(acl.at_cols.loc[:,'x_fit'], acl.at_cols.loc[:,'y_fit'],
-            c=color_list, s=4, cmap='RdYlGn')
 #%%
 """Plot Column positions with color indexing"""
 acl.plot_atom_column_positions(filter_by='elem', sites_to_fit='all',
                                fit_or_ref='fit', 
-                               plot_masked_image=False)
+                               plot_masked_image=True)
+
+#%%
+"""Plot displacements from reference lattice"""
+acl.plot_disp_vects(filter_by='elem', sites_to_plot='all', titles=None,
+                        x_crop=[0, acl.w], y_crop=[acl.h, 0],
+                        scalebar=True, scalebar_len_nm=2,
+                        max_colorwheel_range_pm=None,
+                        plot_fit_points=False, plot_ref_points=False)
 
 #%%
 """Rotate the image and data to align a desired basis vector to horizontal
@@ -116,4 +118,11 @@ acl_rot = acl.rotate_image_and_data(align_basis='a1', align_dir='horizontal')
 acl_rot.plot_atom_column_positions(filter_by='elem', sites_to_fit='all',
                                    fit_or_ref='fit', 
                                    plot_masked_image=False)
-    
+
+#%%
+"""Plot displacements from reference lattice"""
+acl_rot.plot_disp_vects(filter_by='elem', sites_to_plot='all', titles=None,
+                        x_crop=[0, acl_rot.w], y_crop=[acl_rot.h, 0],
+                        scalebar=True, scalebar_len_nm=2,
+                        max_colorwheel_range_pm=10,
+                        plot_fit_points=False, plot_ref_points=False)
