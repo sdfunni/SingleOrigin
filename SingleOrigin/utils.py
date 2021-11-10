74d4ae35f02794d809df5dd28afe24308fd4c54f
sdfunni <60749073+sdfunni@users.noreply.github.com>
2021-11-09 23:14:09 -0500

    Update utils.py

--- SingleOrigin/utils.py
+++ SingleOrigin/utils.py
@@ -8,12 +8,16 @@ from scipy.ndimage.filters import (gaussian_filter, gaussian_laplace,
 from scipy.optimize import minimize
 
 import imageio
+from ncempy.io.dm import dmReader 
+from ncempy.io.ser import serReader
+from ncempy.io.emdVelox import emdVeloxReader
 
 from skimage.segmentation import watershed
 
 import cv2
 
 from matplotlib import pyplot as plt
+
 
 #%%
 """General Crystallographic computations"""
@@ -198,13 +202,12 @@ def import_image(path, display_image=True):
     Parameters
     ----------
     path : str
-         The location of the image file
+         The location of the image file. Supports .dm3/.dm4, .emd, .ser file 
+         types in addition to open source formats such as .tif and .png (all 
+         formats supported by imageio.imread()).
     display_image : bool
         Whether to plot image after importing.
         Default: True 
-    xlim, ylim: array_like of shape (2,)
-        The x and y limits if cropping is desired.
-        Default: None
          
     Returns
     -------
@@ -213,7 +216,21 @@ def import_image(path, display_image=True):
         
     """
     
-    image = imageio.imread(path).astype('float64')
+    if path[-3:] in ['dm4', 'dm3']:
+        dm_file = dmReader(path)
+        image = dm_file['data']
+        
+    elif path[-3:] == 'emd':
+        emd_file = emdVeloxReader(path)
+        image = emd_file['data']
+        
+    elif path[-3:] == 'ser':
+        ser_file = serReader(path)
+        image = ser_file['data']
+    
+    else:
+        image = imageio.imread(path).astype('float64')
+    
     image = image_norm(image)
 
     if display_image == True:
@@ -242,6 +259,7 @@ def image_norm(image):
     [min_, max_] = [np.min(image), np.max(image)]
     image_normed = (image - min_)/(max_ - min_)
     return image_normed
+  
     
 def img_equ_ellip(image):
     """Calculate the equivalent ellipse
@@ -277,6 +295,7 @@ def img_equ_ellip(image):
     eigvects = np.take_along_axis(eigvects, np.array([ind_sort,ind_sort]), 1)
     return eigvals, eigvects, x0, y0
 
+
 def img_ellip_param(image):
     """Find parameters of the equivalent ellipse
     
@@ -308,6 +327,7 @@ def img_ellip_param(image):
                                            eigvects[:, major])))
     
     return x0, y0, eccen, theta, sig_1, sig_2
+
 
 def gaussian_2d(x, y, x0, y0, sig_1, sig_2, ang, A=1, I_o=0):
     """Sample a 2D, ellpitical Gaussian function.
@@ -341,7 +361,8 @@ def gaussian_2d(x, y, x0, y0, sig_1, sig_2, ang, A=1, I_o=0):
     
     return I
 
-def gaussian2d_ss(p0, x, y, z, img_shape):
+
+def gaussian2d_ss(p0, x, y, z, masks=None):
     """Sum of squares for a Gaussian function.
     
     Takes a parameter vector, coordinates, and corresponding data values;  
@@ -349,14 +370,19 @@ def gaussian2d_ss(p0, x, y, z, img_shape):
     
     Parameters
     ----------
-    p0 : array_like with shape (7,)
-        The Gaussian function parameter vector:
+    p0 : array_like with shape (n,7)
+        n = number of peaks to fit
+        Array containing the Gaussian function parameter vector(s):
             [x0, y0, sig_1, sig_2, ang, A, I_o]
-    
     x, y : 1D array_like, must have the same shape
-        They x and y coordinates of image pixels
+        The flattened arrays of x and y coordinates of image pixels
     z : 1D array_like, must have the same shape as x and y
-        The image values at the x, y coordinates
+        The flattened array of image values at the x, y coordinates
+    masks : 2d array_like of size (n, m)
+        n = number of peaks to fit
+        m = number of unmasked pixels
+        The flattened masks for each peak. Each of the "n" rows is 1 where the  
+        mask for the corresponding peak and 0 elsewhere.
          
     Returns
     -------
@@ -365,45 +391,33 @@ def gaussian2d_ss(p0, x, y, z, img_shape):
     
     """
     
-    p0 = p0.flatten()
-    num_gauss = p0.shape[0]//7
-    
-    R = copy.deepcopy(z)
-    
-    p0 = p0.reshape((num_gauss, 7))
-    
+    p0 = p0.reshape((-1, 7))
+    #Initialize model by getting background values:
     if p0.shape[0] > 1:
-        x0y0 = p0[:,:2]
-        image = np.zeros(img_shape)
-        image[y,x] = z
-        masks_labeled, _ = ndimage.label(image)
-        masks_to_peaks = ndimage.map_coordinates(masks_labeled, 
-                                                 np.flipud(x0y0.T), 
-                                                 order=0).astype(int)
-        bkgd = np.zeros(image.shape)
-        for i, mask_num in enumerate(masks_to_peaks):
-            bkgd += np.where(masks_labeled == mask_num, 1, 0) * p0[i,-1]
-        
-        bkgd = bkgd.ravel()
-        unmasked_data = np.nonzero(masks_labeled.ravel())
-        bkgd = np.take(bkgd, unmasked_data)
-        
-        R -= bkgd
+        if type(masks) == type(None):
+            raise Exception('Must specify "masks" arg for fitting '
+                            +'simultaneous peaks')
+        model = np.sum(masks * p0[:,-1].reshape((-1,1)), axis=0)
         
     else:
-        R -= p0[0, -1]
-    for p0_ in p0:
-        [x0, y0, sig_1, sig_2, ang, A, _] = p0_
-        R -= (A*np.exp(-1/2*(((np.cos(ang) * (x - x0)
-                               + np.sin(ang) * (y - y0)) / sig_1)**2
-                             +((-np.sin(ang) * (x - x0) 
-                                + np.cos(ang) * (y - y0)) / sig_2)**2)))
+        model = p0[0, -1]
     
+    #Add Gaussians:
+    x0, y0, sig_1, sig_2, ang, A, _ = np.split(p0, 7, axis=1)
+    model += np.sum(A*np.exp(-1/2*(((np.cos(ang) * (x - x0)
+                                     + np.sin(ang) * (y - y0)) / sig_1)**2
+                                   +((-np.sin(ang) * (x - x0) 
+                                      + np.cos(ang) * (y - y0)) / sig_2)**2)), 
+                    axis=0)
+    
+    #Subtract from data to get residuals:
+    R = z - model
     r_sum_sqrd = (R @ R.T).flatten()
     
     return r_sum_sqrd
 
-def fit_gaussian2D(data, p0, method='L-BFGS-B'):
+
+def fit_gaussian2D(data, p0, masks=None, method='BFGS'):
     """Fit a 2D Gaussain function to data.
     
     Fits a 2D, elliptical Gaussian to an image. Zero values are ignored.
@@ -427,17 +441,17 @@ def fit_gaussian2D(data, p0, method='L-BFGS-B'):
     
     """
        
-    if method not in ['L-BFGS-B', 'Powell', 'trust-constr']:
-       raise Exception('only methods "L-BFGS-B", "Powell" and "trust-constr"'
+    if method not in ['BFGS', 'Powell', 'trust-constr']:
+       raise Exception('only methods "BFGS"'
                         + ' are supported')
     p0 = p0.flatten()
     num_gauss = p0.shape[0]//7
+    img_shape = data.shape
     
     p0_ = p0.reshape((num_gauss, 7))
     
     p0_[:, 4] *= -1
     
-    img_shape = data.shape
     y, x = np.indices(img_shape)
     z=data.ravel()
     
@@ -446,16 +460,34 @@ def fit_gaussian2D(data, p0, method='L-BFGS-B'):
     x = np.take(x, unmasked_data)
     y = np.take(y, unmasked_data)
     
-    bounds = [(-np.inf, np.inf), (-np.inf, np.inf), (1, np.inf), (1, np.inf), 
-              (-4*np.pi, 4*np.pi), (0,2), (0,1)] * num_gauss 
+    x0y0 = p0_[:,:2]    
+    if type(masks) == type(None):
+        image = np.zeros(img_shape)
+        image[y,x] = z
+        masks_labeled, _ = ndimage.label(image)
+    
+    elif (type(masks) == imageio.core.util.Array 
+          or type(masks) == np.ndarray):
+        masks_labeled, _ = ndimage.label(masks)
+        
+    masks_to_peaks = ndimage.map_coordinates(masks_labeled, 
+                                             np.flipud(x0y0.T), 
+                                             order=0).astype(int)
+    masks_labeled = np.take(masks_labeled, unmasked_data).ravel()
+    
+    masks_labeled = np.array([np.where(masks_labeled == mask_num, 1, 0)
+                              for i, mask_num in enumerate(masks_to_peaks)])
+    
+    # bounds = [(-np.inf, np.inf), (-np.inf, np.inf), (1, np.inf), (1, np.inf), 
+    #           (-4*np.pi, 4*np.pi), (0,2), (0,1)] * num_gauss 
     
     p0_ = p0_.flatten()
     
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', category=UserWarning, 
                                 lineno=182)
-        params = minimize(gaussian2d_ss, p0_, args=(x, y, z, img_shape), 
-                          bounds=bounds, method = method).x
+        params = minimize(gaussian2d_ss, p0_, args=(x, y, z, masks_labeled), 
+                          bounds=None, method=method).x
     
     params = params.reshape((-1, 7))
     
@@ -484,6 +516,7 @@ def pcf_radial(dr, coords, total_area=None):
         Histogram of point-point distances with bin size dr
     
     """
+    
     if total_area == None:
         diag_vect = np.max(coords, axis=0) - np.min(coords, axis=0)
         total_area = diag_vect @ np.ones(diag_vect.shape)
@@ -505,9 +538,42 @@ def pcf_radial(dr, coords, total_area=None):
     pcf = hist / (n * rho * A_sh)
     
     return pcf
+
+
+def detect_peaks(image, min_dist=4, thresh=0):
+    """Segment an image using the Watershed algorithm.
     
-def watershed_segment(image, sigma=None, buffer=0, edge_max_threshold = 0.95,
-                      watershed_line=True):
+    Parameters
+    ----------
+    image : 2D array_like
+        The image to be analyzed.
+    min_dist : int or float
+        The minimum distance allowed between detected peaks. Used to create
+        a circular neighborhood kernel for peak detection.
+    thresh : int or float
+        The minimum image value that should be considered a peak. Used to 
+        remove low intensity background noise peaks.
+         
+    Returns
+    -------
+    peaks : 2D array_like with shape: image.shape
+        Array with 1 indicating peak pixels and 0 elsewhere.
+        
+    """
+    
+    kern_rad = int(np.ceil(min_dist))
+    size = 2*kern_rad + 1
+    neighborhood = np.array([1 if np.hypot(i-kern_rad,j-kern_rad) <= min_dist 
+                             else 0 
+                             for j in range(size) for i in range(size)]
+                            ).reshape((size,size))
+    peaks = (maximum_filter(image,footprint=neighborhood)==image
+             ) * (image > thresh)
+    return peaks.astype(int)
+
+
+def watershed_segment(image, sigma=None, buffer=0, local_thresh_factor = 0.95,
+                      watershed_line=True, min_dist=4):
     """Segment an image using the Watershed algorithm.
     
     Parameters
@@ -518,14 +584,17 @@ def watershed_segment(image, sigma=None, buffer=0, edge_max_threshold = 0.95,
         The Laplacian of Gaussian sigma value to use for peak sharpening
     buffer : int
         The border within which peaks are ignored
-    edge_max_threshold : float
+    local_thresh_factor : float
         Removes background from each segmented region by thresholding. 
         Threshold value determined by finding the maximum value of edge pixels
         in the segmented region and multipling this value by the 
-        edge_max_threshold value. The filtered image is used for this 
+        local_thresh_factor value. The filtered image is used for this 
         calculation. Default 0.95.
     watershed_line : bool
         Seperate segmented regions by one pixel. Default True.
+    min_dist : int or float
+        The minimum distance allowed between detected peaks. Used to create
+        a circular neighborhood kernel for peak detection.
          
     Returns
     -------
@@ -541,24 +610,25 @@ def watershed_segment(image, sigma=None, buffer=0, edge_max_threshold = 0.95,
     img_der = copy.deepcopy(image)
     [h, w] = image.shape
     
-    if type(sigma) is (int or float or tuple):
+    if type(sigma) in (int, float, tuple):
+        print('log...')
         img_der = image_norm(-gaussian_laplace(img_der, sigma))
-
-    neighborhood = np.ones((9,9))
-    local_max, _ = ndimage.label(maximum_filter(img_der,
-                                footprint=neighborhood)==img_der)
+        # img_der = image_norm(-gaussian_laplace(img_der, sigma))
+        
+    local_max, _ = ndimage.label(detect_peaks(image, min_dist=min_dist))
+    
     masks = watershed(-img_der,local_max, watershed_line=watershed_line)
     slices = ndimage.find_objects(masks)
     num_masks = int(np.max(masks))
     
     masks_ref = np.zeros(image.shape)
     
-    """Refine masks with edge_max_threshold"""
+    """Refine masks with local_thresh_factor"""
     for i in range(num_masks):
         mask_sl = np.where(masks[slices[i][0],slices[i][1]]== i+1, 1, 0)
         img_der_sl = img_der[slices[i][0],slices[i][1]]
         edge = mask_sl - ndimage.morphology.binary_erosion(mask_sl)
-        thresh = np.max(edge*img_der_sl) * edge_max_threshold
+        thresh = np.max(edge*img_der_sl) * local_thresh_factor
         mask_sl = np.where(mask_sl*img_der_sl > thresh, 1, 0)
         masks_ref[slices[i][0],slices[i][1]] += mask_sl * (i+1)
         
